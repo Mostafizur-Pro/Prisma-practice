8d8b7c66c19320cff19cf1b84f07b6f61f2a55d9
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-08-12 16:06:03 +0600

    create users

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,10 @@ import { PrismaClient } from "@prisma/client";
 import app from "./app";
 
 const prisma = new PrismaClient();
-
 const port = process.env.PORT || 3003;
 async function main() {
   app.listen(port, () => {
-    console.log(`Server running at ${port}}`);
+    console.log(`Server running at ${port}`);
   });
 }
 
---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+
+import { UserService } from "./user.service";
+
+const insertIntoDB = async (req: Request, res: Response) => {
+  try {
+    const result = await UserService.insertIntoDB(req.body);
+    res.send({
+      success: true,
+      message: "User create Successfully!",
+      data: result,
+    });
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+export const UserController = {
+  insertIntoDB,
+};
