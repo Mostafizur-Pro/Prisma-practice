e6a55c4d3efd03531613acc89dee4cde9344a903
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-08-12 16:49:28 +0600

    create or update profile conditionally

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -15,6 +15,21 @@ const insertIntoDB = async (req: Request, res: Response) => {
   }
 };
 
+const insertOrUpdateProfile = async (req: Request, res: Response) => {
+  try {
+    const result = await UserService.insertOrUpdateProfile(req.body);
+
+    res.send({
+      success: true,
+      message: "Profile create/update Successfully!",
+      data: result,
+    });
+  } catch (err) {
+    res.send(err);
+  }
+};
+
 export const UserController = {
   insertIntoDB,
+  insertOrUpdateProfile,
 };
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,39 @@
-import express from "express";
-import { UserController } from "./user.controller";
+import { PrismaClient, Profile, User } from "@prisma/client";
 
-const router = express.Router();
+const prisma = new PrismaClient();
 
-router.post("/create-user", UserController.insertIntoDB);
+const insertIntoDB = async (data: User): Promise<User> => {
+  const result = await prisma.user.create({
+    data,
+  });
+  return result;
+};
 
-export const UserRoutes = router;
+const insertOrUpdateProfile = async (data: Profile): Promise<Profile> => {
+  const isExist = await prisma.profile.findUnique({
+    where: {
+      userId: data.userId,
+    },
+  });
+  if (isExist) {
+    const result = await prisma.profile.update({
+      where: {
+        userId: data.userId,
+      },
+      data: {
+        bio: data.bio,
+      },
+    });
+    return result;
+  }
+
+  const result = await prisma.profile.create({
+    data,
+  });
+  return result;
+};
+
+export const UserService = {
+  insertIntoDB,
+  insertOrUpdateProfile,
+};
